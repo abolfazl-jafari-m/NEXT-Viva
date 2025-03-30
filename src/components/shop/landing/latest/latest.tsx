d55c11f12270e6a335d6abe7a5fe655e80ec0fbb
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-03-30 18:48:28 +0330

    feat(landing) : latest section is Designed

---
+++ src/components/shop/landing/latest/latest.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from "next/image";
+import tomford from "@/assets/images/category-women-sweet.png"
+import bitter from "@/assets/images/category-womens-bitter.png"
+import cool from "@/assets/images/category-mens-cool.png"
+
+
+function Latest() {
+    return (
+        <section className={"w-full h-full p-10 "}>
+            <h2 className={"text-4xl text-white font-bold"}>آخرین محصولات</h2>
+            <div className={"grid grid-cols-4  mt-13  gap-8 gap-y-12"}>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={tomford.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={bitter.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={tomford.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={bitter.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={cool.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={bitter.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={tomford.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+                <div className={"flex flex-col gap-3 items-center"}>
+                    <Image src={cool.src} alt={"tom ford"} width={180} height={180}/>
+                    <h2 className={"text-gold"}>عطر تام فورد فاکینگ فبیولس</h2>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+
+export default Latest;
