93abf3637953c9f86bce24479303875353f0270d
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-03-31 13:03:58 +0330

    feat(landing) : footer added

---
+++ src/components/shop/shared/footer/footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from "next/image";
+import logo from "@/assets/images/logo.png"
+import {IoIosArrowUp} from "react-icons/io";
+
+function Footer() {
+    return (
+        <footer className={"w-full py-2  flex items-center justify-around "}>
+            <div>
+                <Image src={logo.src} alt={"logo"} width={72} height={72}/>
+            </div>
+            <div className={"flex flex-col gap-3 items-center justify-center"}>
+                <ul className={"w-full flex items-center gap-12 list-disc text-darkerGold justify-center"}>
+                    <li>خانه</li>
+                    <li>محصولات</li>
+                    <li>تماس با ما</li>
+                    <li>درباره ما</li>
+                </ul>
+                <p className={"text-white "} dir={"ltr"}><span
+                    className={"text-lg text-gold mx-3 font-mono"}>@NV Design</span>طراحی توسط تیم </p>
+            </div>
+            <div className={" text-white font-bold"}>
+                <IoIosArrowUp size={42}/>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
