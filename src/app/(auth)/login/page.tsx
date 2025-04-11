ba9b423513479345319a7932bedb40584a3cde28
Abolazl Jafari M <abolfazljafari563@gmail.com>
2025-04-11 10:30:03 +0330

    feat (auth ) : responsive login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/auth/loginForm/LoginForm";
 
 function Login() {
     return (
-        <div className={"bg-primary/70 backdrop-blur-lg rounded-xl p-10  w-2/6 shadow-2xl shadow-black flex flex-col items-center gap-10"}>
+        <div className={"bg-primary/70 backdrop-blur-lg rounded-xl p-10 max-sm:p-4  w-2/6 shadow-2xl shadow-black flex flex-col items-center gap-10 max-lg:w-2/4 max-sm:w-3/4"}>
             <h2 className={"text-2xl font-semibold text-darkerGold "}>صفحه ورود</h2>
             <LoginForm />
         </div>
