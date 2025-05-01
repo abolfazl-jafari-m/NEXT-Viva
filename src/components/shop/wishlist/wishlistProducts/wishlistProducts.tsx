e3b5309a50cca48b6570138a56a676a2a85e104b
AbolfazlJM <abolfazljafari563@gmail.com>
2025-05-01 19:40:03 +0330

    fix (shop /wishlist) : change promise add fetch to useQueries

--- src/components/shop/wishlist/wishlistProducts/wishlistProducts.tsx
+++ src/components/shop/wishlist/wishlistProducts/wishlistProducts.tsx
@@ -1,29 +1,25 @@
 "use client";
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useWishlistStore} from "@/lib/providers/WishListStoreProvider";
-import {Product} from "@/interfaces/interfaces";
 import {getProductById} from "@/services/products";
 import {PuffLoader} from "react-spinners";
 import WishlistProductCard from "@/components/shop/wishlist/wishlistProductCard/wishlistProductCard";
 import {useTranslations} from "next-intl";
+import {useQueries} from "@tanstack/react-query";
 
 function WishlistProducts() {
     const t = useTranslations("wishlist")
     const favorites = useWishlistStore(state => state.products);
-    const [products, setProducts] = useState<Product[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const data = useQueries({
+        queries: favorites.map(item => {
+            return {
+                queryKey: ['favorite', item],
+                queryFn: () => getProductById(item),
+            }
+        })
+    })
 
-    useEffect(() => {
-        if (favorites.length > 0) {
-            Promise.all(favorites.map(item => getProductById(item)))
-                .then(res => setProducts(res))
-                .catch(e => console.log(e))
-                .finally(() => setIsLoading(false));
-        } else {
-            setIsLoading(false);
-            setProducts([]);
-        }
-    }, [favorites])
+    const isLoading = data.some(item => item.isLoading);
 
     if (isLoading) {
         return (
@@ -34,8 +30,8 @@ function WishlistProducts() {
     return (
         <div className={"flex flex-col gap-5 w-full h-full max-sm:items-center"}>
             {
-                products.length > 0 ?
-                    products.map((product) => <WishlistProductCard key={product.id} product={product}/>)
+                data.length > 0 ?
+                    data.map((product) => <WishlistProductCard key={product.data.id} product={product.data}/>)
                     :
                     <div className={"flex items-center justify-center w-full h-full pt-10"}>
                         <p className={"text-white/70 font-semibold text-xl text-center max-sm:text-[16px] light:text-black"}>{t("noFav")}</p>
