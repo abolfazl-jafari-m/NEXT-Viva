6eeb3754978228cc128f0fd95f20b822df051e01
AbolfazlJM <abolfazljafari563@gmail.com>
2025-05-08 23:24:19 +0330

    fix (shop /search ) : add loading to search

--- src/components/shop/cart/cartProductCard/cartProductCard.tsx
+++ src/components/shop/cart/cartProductCard/cartProductCard.tsx
@@ -16,9 +16,8 @@ function CartProductCard({cartItem}: { cartItem: CartItem }) {
     const dec = useCartStore((state) => state.dec);
     const inc = useCartStore((state) => state.inc);
 
-    const increment = ()=>{
-        console.log(cartItem)
-        if (cartItem.quantity < cartItem.inventory){
+    const increment = () => {
+        if (+cartItem.quantity < +cartItem.inventory) {
             inc(cartItem.id);
         }
     }
@@ -30,8 +29,9 @@ function CartProductCard({cartItem}: { cartItem: CartItem }) {
                 <CgClose className={"text-2xl text-white cursor-pointer"} onClick={() => remove(cartItem.id)}/>
                 <Link href={`/products/${cartItem.id}`}>
                     <div className={"flex gap-2"}>
-                        <Image src={`http://api.alikooshesh.ir:3000${cartItem.image}`} alt={cartItem.title} width={50} height={50}
-                             className={"rounded-lg  w-14 max-sm:w-17"}/>
+                        <Image src={`http://api.alikooshesh.ir:3000${cartItem.image}`} alt={cartItem.title} width={50}
+                               height={50}
+                               className={"rounded-lg  w-14 max-sm:w-17"}/>
                         <div className={"flex flex-col gap-2 text-white"}>
                             <h4 className={"text-xl max-sm:text-[16px]"}>{cartItem.title}</h4>
                             <p>{cartItem.volume} {t("mil")}</p>
@@ -39,7 +39,8 @@ function CartProductCard({cartItem}: { cartItem: CartItem }) {
                     </div>
                 </Link>
             </div>
-            <div className={"flex items-center gap-6 max-lg:flex-col-reverse max-lg:items-center max-sm:flex-row max-sm:justify-between max-sm:w-full"}>
+            <div
+                className={"flex items-center gap-6 max-lg:flex-col-reverse max-lg:items-center max-sm:flex-row max-sm:justify-between max-sm:w-full"}>
                 <div className={"flex items-center gap-2 min-w-26 max-sm:min-w-fit max-lg:justify-center "}>
                     <Button onClick={() => increment()}
                             className={"rounded-full bg-secondary p-1 ring ring-primary text-primary text-xs cursor-pointer"}
--- src/components/shop/shared/search/search.tsx
+++ src/components/shop/shared/search/search.tsx
@@ -8,21 +8,29 @@ import {Product} from "@/interfaces/interfaces";
 import Link from "next/link";
 import Image from "next/image";
 import {useTranslations} from "next-intl";
+import {BeatLoader} from "react-spinners";
 
 function Search() {
     const t = useTranslations("search");
     const [isOpen, setIsOpen] = useState(false);
     const [products, setProducts] = useState<Product[]>([]);
     const [value, setValue] = useState<string>("");
+    const [loading, setLoading] = useState(false);
     const debounce = useDebouncedCallback((value: string) => {
         if (value !== "") {
-            searchProducts(value).then((res) => setProducts(res))
+            setLoading(true);
+            searchProducts(value)
+                .then((res) => setProducts(res))
+                .catch((err) => console.error(err))
+                .finally(() => setLoading(false));
         }
         setValue(value);
+        setProducts([]);
     }, 1000);
     return (
         <div>
-            <CgSearch  onClick={() => setIsOpen(true)} className={"cursor-pointer text-2xl max-sm:text-xl light:max-md:text-black"}/>
+            <CgSearch onClick={() => setIsOpen(true)}
+                      className={"cursor-pointer text-2xl max-sm:text-xl light:max-md:text-black"}/>
             <AnimatePresence>
                 {
                     isOpen && (
@@ -32,13 +40,19 @@ function Search() {
                                         className={"bg-primary/80 fixed top-0 left-0 w-full h-screen backdrop-blur-2xl z-20 flex items-center justify-center flex-col gap-4"}>
                             <span
                                 className={"absolute top-10 left-10 rounded-full bg-darkerGold text-white p-1 cursor-pointer"}
-                                onClick={() => setIsOpen(false)}>
+                                onClick={() => {
+                                    setIsOpen(false);
+                                    setProducts([])
+                                    setValue("");
+                                }}>
                                 <CgClose/>
                             </span>
-                                <div className={"bg-secondary rounded-lg ring ring-gold px-3 py-2 w-1/3 max-lg:w-2/3 max-md:w-3/4"}>
-                                    <input className={"w-full outline-none border-none bg-transparent text-black"}
+                                <div
+                                    className={"bg-secondary rounded-lg ring ring-gold px-3 py-2 w-1/3 max-lg:w-2/3 max-md:w-3/4 flex items-center gap-2"}>
+                                    <input className={"w-full outline-none border-none bg-transparent text-black flex-1"}
                                            onChange={(e) => debounce(e.target.value)}
                                            placeholder={t("search")}/>
+                                    {loading && <BeatLoader size={12}/>}
                                 </div>
                                 <div
                                     className={"h-75 w-1/3 max-lg:w-2/3 max-md:w-3/4 overflow-y-auto no-scrollbar flex flex-col gap-4 px-4 py-2"}>
@@ -47,17 +61,20 @@ function Search() {
                                             products.map((product: Product) => (
                                                 <div key={product.id}
                                                      className={"flex items-center gap-3 rounded-md bg-black  ring ring-zinc-700 p-2"}>
-                                                    <Image src={`http://api.alikooshesh.ir:3000${product.images[0]}`} width={55} height={55} alt={product.slug}
-                                                         className={"h-14 w-14 rounded-lg shadow shadow"}/>
+                                                    <Image src={`http://api.alikooshesh.ir:3000${product.images[0]}`}
+                                                           width={55} height={55} alt={product.slug}
+                                                           className={"h-14 w-14 rounded-lg shadow shadow"}/>
                                                     <div className={"flex flex-col gap-2 text-white"}>
-                                                        <Link href={`/products/${product.id}`} onClick={() => {setIsOpen(false)}}>
+                                                        <Link href={`/products/${product.id}`} onClick={() => {
+                                                            setIsOpen(false)
+                                                        }}>
                                                             <h3 className={"text-lg max-sm:text-sm"}>{product.title}</h3>
                                                         </Link>
                                                         <p>{product.price}</p>
                                                     </div>
                                                 </div>))
                                             :
-                                            value !== "" &&
+                                            value !== "" && !loading &&
                                             <p className={"text-center"}>{t("noFounded")}</p>
                                     }
                                 </div>
