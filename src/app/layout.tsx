82db0c8b5d7215be6242050da54691b32f2db2d8
AbolfazlJM <abolfazljafari563@gmail.com>
2025-05-18 09:38:56 +0330

    fix (assets ) : move fonts to public directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,37 +10,18 @@ import {CartStoreProvider} from "@/lib/providers/CartStoreProivder";
 
 const vazir = localFont({
     src: [
-        {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-thin.woff2",
-            weight: "100",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-ExtraLight.woff2",
-            weight: "200",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-Light.woff2",
-            weight: "300",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-Regular.woff2",
-            weight: "400",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-Medium.woff2",
-            weight: "500",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-SemiBold.woff2",
-            weight: "600",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-Bold.woff2",
-            weight: "700",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-ExtraBold.woff2",
-            weight: "800",
-        }, {
-            path: "../assets/fonts/vazir/Vazirmatn-UI-FD-Black.woff2",
-            weight: "900",
-        },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-thin.woff2", weight: "100" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-ExtraLight.woff2", weight: "200" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-Light.woff2", weight: "300" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-Regular.woff2", weight: "400" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-Medium.woff2", weight: "500" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-SemiBold.woff2", weight: "600" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-Bold.woff2", weight: "700" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-ExtraBold.woff2", weight: "800" },
+        { path: "/fonts/vazir/Vazirmatn-UI-FD-Black.woff2", weight: "900" },
     ],
-    variable: "--font-vazir"
-})
+    variable: "--font-vazir",
+});
 
 export const metadata: Metadata = {
     title: "ویوا عطر",
